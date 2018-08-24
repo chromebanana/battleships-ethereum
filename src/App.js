c6f586fe196def01e3e32df368754cd45e2f214d
William Cragg <william.cragg@arrkgroup.com>
2018-08-24 20:04:17 +0100

    functioning, stripped down App.js

--- src/App.js
+++ src/App.js
@@ -29,49 +29,32 @@ class App extends Component {
     .then(results => {
       this.setState({
         web3: results.web3
-      })
+      });
+      const contract = require('truffle-contract')
+      const battleships = contract(Battleships)
+      battleships.setProvider(this.state.web3.currentProvider)
 
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
+//Get accounts
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      battleships.deployed().then((instance) => {
+        this.setState({
+          account: accounts[0],
+          contractInstance: instance
+        });
+        console.log('account::::', this.state.account)
+      });
+    });
+
+  })
     .catch(() => {
       console.log('Error finding web3.')
     })
   }
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(Battleships)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    // this.state.event = battleships.GameInitialized();
 
-    // this.state.event = simpleStorage.GameInitialized();
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0], contract: simpleStorageInstance, account: accounts[0] })
-      })
-    })
-  }
 
   createGame(event){
     const contract = this.state.contract
@@ -120,7 +103,7 @@ class App extends Component {
               <h2>Smart Contract Example</h2>
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
               <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <p>Your account is: {this.state.account}</p>
               <button onClick={this.createGame.bind(this)}>Create Game</button>
               <button onClick={this.getOpenGames.bind(this)}>See All Open Games</button>
               <p> Open Games are: {this.state.openGameIds}</p>
